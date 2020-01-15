43a1732e6ce2f5933c4ff8bfa750559576aa0dfb
gkmille2 <gkmille2@asu.edu>
2020-01-15 11:17:47 -0700

    Initial commit

---
+++ src/assets/scripts/index.js
@@ -0,0 +1,17 @@
+import '../styles/index.scss';
+
+import './masonry';
+import './charts';
+import './popover';
+import './scrollbar';
+import './search';
+import './sidebar/index.js';
+import './skycons';
+import './vectorMaps';
+import './chat';
+import './datatable';
+import './datepicker';
+import './email';
+import './fullcalendar';
+import './googleMaps';
+import './utils';
