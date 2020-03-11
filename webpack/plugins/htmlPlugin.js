412ad2f972561da50688b4055e741c22dd5ea578
idhant96 <idhant123@gmail.com>
2020-03-11 14:36:51 -0700

    completed hrefs to records

--- webpack/plugins/htmlPlugin.js
+++ webpack/plugins/htmlPlugin.js
@@ -60,7 +60,8 @@ const titles = {
   'profile-image-selection-admin': 'Select Profile Image',
   'add-user-admin': 'Add a User',
   'manage-groups-admin': 'Manage Groups',
-  'add-group-admin': 'Add a Group'
+  'add-group-admin': 'Add a Group',
+  'caretaker': 'caretaker'
 }
 
 module.exports = Object.keys(titles).map(title => {
