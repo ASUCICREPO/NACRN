810bbde1bfac3111e2af035a941523e0fff624ba
Akhila5diddi <akhila5netha@gmail.com>
2020-05-11 14:45:27 -0700

    add changes

--- src/assets/scripts/determineState/index.js
+++ src/assets/scripts/determineState/index.js
@@ -35,6 +35,13 @@ let adminBar = `<li class="nav-item mT-30 actived">
 </a>
 </li>
 <li class="nav-item mT-30 actived">
+<a class="sidebar-link" href="form-management-admin.html">
+  <span class="icon-holder">
+  <i class="c-blue-500 fas fa-clipboard"></i>
+  </span>
+  <span class="title">Form Management</span>
+</a>
+<li class="nav-item mT-30 actived">
 <a class="sidebar-link" href="invite-admin.html">
   <span class="icon-holder">
     <i class="c-blue-500 fab fa-telegram-plane"></i>
@@ -101,6 +108,14 @@ let powerUserBar = `<li class="nav-item mT-30 actived">
   <span class="title">Care Coordinator</span>
 </a>
 </li>
+<li class="nav-item mT-30 actived">
+<a class="sidebar-link" href="form-management-powerUser.html">
+  <span class="icon-holder">
+   
+    <i class="c-blue-500 fas fa-clipboard"></i>
+  </span>
+  <span class="title">Form Management</span>
+</a>
 
 <li class="nav-item mT-30 actived">
 <a class='sidebar-link' href="powerUserChat.html">
@@ -120,6 +135,57 @@ let powerUserBar = `<li class="nav-item mT-30 actived">
 </li>
 <li class="nav-item mT-30 actived">
 <a class='sidebar-link' href="calendar_powerUser.html">
+  <span class="icon-holder">
+    <i class="c-blue-500 fas fa-calendar-check"></i>
+  </span>
+  <span class="title">Schedule Appointment</span>
+</a>
+</li>
+`
+let careCoordinatorBar = `<li class="nav-item mT-30 actived">
+<a class="sidebar-link" href="careCoordinator_home.html">
+  <span class="icon-holder">
+    <i class="c-blue-500 fas fa-home"></i>
+  </span>
+  <span class="title">Home</span>
+</a>
+</li>
+<li class="nav-item mT-30 actived">
+<a class="sidebar-link" href="index_careCoordinator.html">
+  <span class="icon-holder">
+    <i class="c-blue-500 fas fa-search"></i>
+  </span>
+  <span class="title">All Programs</span>
+</a>
+</li>
+
+<li class="nav-item mT-30 actived">
+<a class='sidebar-link' href="caretaker-careCoordinator.html">
+  <span class="icon-holder">
+  <i class="c-blue-500 fas fa-hand-holding-medical"></i>
+  </span>
+  <span class="title">Care Coordinator</span>
+</a>
+</li>
+
+<li class="nav-item mT-30 actived">
+<a class='sidebar-link' href="careCoordinatorChat.html">
+  <span class="icon-holder">
+    <i class="c-blue-500 fas fa-comments"></i>
+  </span>
+  <span class="title">Chat</span>
+</a>
+</li>
+<li class="nav-item mT-30 actived">
+<a class="sidebar-link" href="invite_careCoordinator.html">
+  <span class="icon-holder">
+    <i class="c-blue-500 fab fa-telegram-plane"></i>
+  </span>
+  <span class="title">Send an Invite</span>
+</a>
+</li>
+<li class="nav-item mT-30 actived">
+<a class='sidebar-link' href="calendar_careCoordinator.html">
   <span class="icon-holder">
     <i class="c-blue-500 fas fa-calendar-check"></i>
   </span>
@@ -260,7 +326,7 @@ let admin_settings = `<div class="header-container">
       <li>
         <ul class="ovY-a pos-r scrollable lis-n p-0 m-0 fsz-sm">
           <li>
-            <a href="adminChat.html" class='peers fxw-nw td-n p-20 bdB c-grey-800 cH-blue bgcH-grey-100'>
+            <a href="email.html" class='peers fxw-nw td-n p-20 bdB c-grey-800 cH-blue bgcH-grey-100'>
               <div class="peer mR-15">
                 <img class="w-3r bdrs-50p" src="https://randomuser.me/api/portraits/men/1.jpg" alt="">
               </div>
@@ -283,7 +349,7 @@ let admin_settings = `<div class="header-container">
           </li>
          
           <li>
-            <a href="adminChat.html" class='peers fxw-nw td-n p-20 bdB c-grey-800 cH-blue bgcH-grey-100'>
+            <a href="email.html" class='peers fxw-nw td-n p-20 bdB c-grey-800 cH-blue bgcH-grey-100'>
               <div class="peer mR-15">
                 <img class="w-3r bdrs-50p" src="https://randomuser.me/api/portraits/men/3.jpg" alt="">
               </div>
@@ -308,7 +374,7 @@ let admin_settings = `<div class="header-container">
       </li>
       <li class="pX-20 pY-15 ta-c bdT">
         <span>
-          <a href="adminChat.html" class="c-grey-600 cH-blue fsz-sm td-n">View All Messages <i class="fs-xs ti-angle-right mL-10"></i></a>
+          <a href="email.html" class="c-grey-600 cH-blue fsz-sm td-n">View All Messages <i class="fs-xs ti-angle-right mL-10"></i></a>
         </span>
       </li>
     </ul>
@@ -332,6 +398,184 @@ let admin_settings = `<div class="header-container">
       </li>
       <li>
         <a href="adminChat.html" class="d-b td-n pY-5 bgcH-grey-100 c-grey-700">
+          <i class="ti-email mR-10"></i>
+          <span>Messages</span>
+        </a>
+      </li>
+      <li role="separator" class="divider"></li>
+      <li>
+        <a href="signin.html" class="d-b td-n pY-5 bgcH-grey-100 c-grey-700">
+          <i class="ti-power-off mR-10"></i>
+          <span>Logout</span>
+        </a>
+      </li>
+      
+    </ul>
+  </li>
+</ul>
+</div>`
+
+let careCoordinator_settings = `<div class="header-container">
+<ul class="nav-left">
+  <li>
+    <a id='sidebar-toggle' class="sidebar-toggle" href="javascript:void(0);">
+      <i class="ti-menu"></i>
+    </a>
+  </li>
+  <li class="search-box">
+    <a class="search-toggle no-pdd-right" href="javascript:void(0);">
+      <i class="search-icon ti-search pdd-right-10"></i>
+      <i class="search-icon-close ti-close pdd-right-10"></i>
+    </a>
+  </li>
+  <li class="search-input">
+    <input class="form-control" type="text" placeholder="Search...">
+  </li>
+</ul>
+<ul class="nav-right">
+  <li class="notifications dropdown">
+    <span class="counter bgc-red">3</span>
+    <a href="" class="dropdown-toggle no-after" data-toggle="dropdown">
+      <i class="ti-bell"></i>
+    </a>
+
+    <ul class="dropdown-menu">
+      <li class="pX-20 pY-15 bdB">
+        <i class="ti-bell pR-10"></i>
+        <span class="fsz-sm fw-600 c-grey-900">Notifications</span>
+      </li>
+      <li>
+        <ul class="ovY-a pos-r scrollable lis-n p-0 m-0 fsz-sm">
+          <li>
+            <a href="notifications-careCoordinator.html" class='peers fxw-nw td-n p-20 bdB c-grey-800 cH-blue bgcH-grey-100'>
+              <div class="peer mR-15">
+                <img class="w-3r bdrs-50p" src="https://randomuser.me/api/portraits/lego/5.jpg" alt="">
+              </div>
+              <div class="peer peer-greed">
+                <span>
+                  <span class="fw-500">Reminder</span>
+                  <span class="c-grey-600"> You have an approintment coming up <span class="text-dark">in 15 Mins</span>
+                  </span>
+                </span>
+                <p class="m-0">
+                  <small class="fsz-xs">5 mins ago</small>
+                </p>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="notifications-careCoordinator.html" class='peers fxw-nw td-n p-20 bdB c-grey-800 cH-blue bgcH-grey-100'>
+              <div class="peer mR-15">
+                <img class="w-3r bdrs-50p" src="https://randomuser.me/api/portraits/lego/5.jpg" alt="">
+              </div>
+              <div class="peer peer-greed">
+                <span>
+                  <span class="fw-500">Reminder</span>
+                  <span class="c-grey-600">Check a new Change made on<span class="text-dark"> Homelessness Program .</span>
+                  </span>
+                </span>
+                <p class="m-0">
+                  <small class="fsz-xs">7 mins ago</small>
+                </p>
+              </div>
+            </a>
+          </li>
+          
+        </ul>
+      </li>
+      <li class="pX-20 pY-15 ta-c bdT">
+        <span>
+          <a href="notifications-careCoordinator.html" class="c-grey-600 cH-blue fsz-sm td-n">View All Notifications <i class="ti-angle-right fsz-xs mL-10"></i></a>
+        </span>
+      </li>
+    </ul>
+  </li>
+  <li class="notifications dropdown">
+    <span class="counter bgc-blue">3</span>
+    <a href="" class="dropdown-toggle no-after" data-toggle="dropdown">
+      <i class="ti-email"></i>
+    </a>
+
+    <ul class="dropdown-menu">
+      <li class="pX-20 pY-15 bdB">
+        <i class="ti-email pR-10"></i>
+        <span class="fsz-sm fw-600 c-grey-900">Messages</span>
+      </li>
+      <li>
+        <ul class="ovY-a pos-r scrollable lis-n p-0 m-0 fsz-sm">
+          <li>
+            <a href="email.html" class='peers fxw-nw td-n p-20 bdB c-grey-800 cH-blue bgcH-grey-100'>
+              <div class="peer mR-15">
+                <img class="w-3r bdrs-50p" src="https://randomuser.me/api/portraits/men/1.jpg" alt="">
+              </div>
+              <div class="peer peer-greed">
+                <div>
+                  <div class="peers jc-sb fxw-nw mB-5">
+                    <div class="peer">
+                      <p class="fw-500 mB-0">John Doe(Care Co-ordinator)</p>
+                    </div>
+                    <div class="peer">
+                      <small class="fsz-xs">5 mins ago</small>
+                    </div>
+                  </div>
+                  <span class="c-grey-600 fsz-sm">
+                    Please look into new change on Homelessness program.
+                  </span>
+                </div>
+              </div>
+            </a>
+          </li>
+         
+          <li>
+            <a href="email.html" class='peers fxw-nw td-n p-20 bdB c-grey-800 cH-blue bgcH-grey-100'>
+              <div class="peer mR-15">
+                <img class="w-3r bdrs-50p" src="https://randomuser.me/api/portraits/men/3.jpg" alt="">
+              </div>
+              <div class="peer peer-greed">
+                <div>
+                  <div class="peers jc-sb fxw-nw mB-5">
+                    <div class="peer">
+                      <p class="fw-500 mB-0">Lee Doe(Care Co-ordinator)</p>
+                    </div>
+                    <div class="peer">
+                      <small class="fsz-xs">25 mins ago</small>
+                    </div>
+                  </div>
+                  <span class="c-grey-600 fsz-sm">
+                    Please let me know your insights on Food For All programs.
+                  </span>
+                </div>
+              </div>
+            </a>
+          </li>
+        </ul>
+      </li>
+      <li class="pX-20 pY-15 ta-c bdT">
+        <span>
+          <a href="email.html" class="c-grey-600 cH-blue fsz-sm td-n">View All Messages <i class="fs-xs ti-angle-right mL-10"></i></a>
+        </span>
+      </li>
+    </ul>
+  </li>
+  <li class="dropdown">
+    <a href="" class="dropdown-toggle no-after peers fxw-nw ai-c lh-1" data-toggle="dropdown">
+      <div class="peer mR-10">
+        <img class="w-2r bdrs-50p" src="https://randomuser.me/api/portraits/men/10.jpg" alt="">
+      </div>
+      <div class="peer">
+        <span class="fsz-sm c-grey-900">John Doe</span>
+      </div>
+    </a>
+    <ul class="dropdown-menu fsz-sm">
+    
+      <li>
+        <a href="settings-careCoordinator.html" class="d-b td-n pY-5 bgcH-grey-100 c-grey-700">
+          <i class="ti-user mR-10"></i>
+          <span>Profile</span>
+        </a>
+      </li>
+      <li>
+        <a href="careCoordinatorChat.html" class="d-b td-n pY-5 bgcH-grey-100 c-grey-700">
           <i class="ti-email mR-10"></i>
           <span>Messages</span>
         </a>
@@ -438,7 +682,7 @@ let powerUser_settings = `<div class="header-container">
       <li>
         <ul class="ovY-a pos-r scrollable lis-n p-0 m-0 fsz-sm">
           <li>
-            <a href="powerUserChat.html" class='peers fxw-nw td-n p-20 bdB c-grey-800 cH-blue bgcH-grey-100'>
+            <a href="email.html" class='peers fxw-nw td-n p-20 bdB c-grey-800 cH-blue bgcH-grey-100'>
               <div class="peer mR-15">
                 <img class="w-3r bdrs-50p" src="https://randomuser.me/api/portraits/men/1.jpg" alt="">
               </div>
@@ -461,7 +705,7 @@ let powerUser_settings = `<div class="header-container">
           </li>
          
           <li>
-            <a href="powerUserChat.html" class='peers fxw-nw td-n p-20 bdB c-grey-800 cH-blue bgcH-grey-100'>
+            <a href="email.html" class='peers fxw-nw td-n p-20 bdB c-grey-800 cH-blue bgcH-grey-100'>
               <div class="peer mR-15">
                 <img class="w-3r bdrs-50p" src="https://randomuser.me/api/portraits/men/3.jpg" alt="">
               </div>
@@ -486,7 +730,7 @@ let powerUser_settings = `<div class="header-container">
       </li>
       <li class="pX-20 pY-15 ta-c bdT">
         <span>
-          <a href="powerUserChat.html" class="c-grey-600 cH-blue fsz-sm td-n">View All Messages <i class="fs-xs ti-angle-right mL-10"></i></a>
+          <a href="email.html" class="c-grey-600 cH-blue fsz-sm td-n">View All Messages <i class="fs-xs ti-angle-right mL-10"></i></a>
         </span>
       </li>
     </ul>
@@ -619,7 +863,7 @@ let endUserSettings = `
           <li>
             <ul class="ovY-a pos-r scrollable lis-n p-0 m-0 fsz-sm">
               <li>
-                <a href="endUserChat.html"  class='peers fxw-nw td-n p-20 bdB c-grey-800 cH-blue bgcH-grey-100'>
+                <a href="email.html"  class='peers fxw-nw td-n p-20 bdB c-grey-800 cH-blue bgcH-grey-100'>
                   <div class="peer mR-15">
                     <img class="w-3r bdrs-50p" src="https://randomuser.me/api/portraits/men/1.jpg" alt="">
                   </div>
@@ -642,7 +886,7 @@ let endUserSettings = `
               </li>
              
               <li>
-                <a href="" class='peers fxw-nw td-n p-20 bdB c-grey-800 cH-blue bgcH-grey-100'>
+                <a href="email.html" class='peers fxw-nw td-n p-20 bdB c-grey-800 cH-blue bgcH-grey-100'>
                   <div class="peer mR-15">
                     <img class="w-3r bdrs-50p" src="https://randomuser.me/api/portraits/men/3.jpg" alt="">
                   </div>
@@ -667,7 +911,7 @@ let endUserSettings = `
           </li>
           <li class="pX-20 pY-15 ta-c bdT">
             <span>
-              <a href="endUserChat.html" class="c-grey-600 cH-blue fsz-sm td-n">View All Messages <i class="fs-xs ti-angle-right mL-10"></i></a>
+              <a href="email.html" class="c-grey-600 cH-blue fsz-sm td-n">View All Messages <i class="fs-xs ti-angle-right mL-10"></i></a>
             </span>
           </li>
         </ul>
@@ -767,9 +1011,9 @@ let form_management_sidenav = `<li class="nav-item mT-30 actived">
 </a>
 </li>`
 
-if (url.includes('form')) {
-  document.querySelector('#sideNavBar').innerHTML = form_management_sidenav
-  document.querySelector('#topBar').innerHTML = admin_settings
+if (url.includes('careCoordinator')) {
+  document.querySelector('#sideNavBar').innerHTML = careCoordinatorBar
+  document.querySelector('#topBar').innerHTML = careCoordinator_settings
 } else if (url.includes('admin')) {
   document.querySelector('#sideNavBar').innerHTML = adminBar
   document.querySelector('#topBar').innerHTML = admin_settings
